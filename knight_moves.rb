81014e438f8829ac9879f0c31add61da5bae0cd1
Duncalicious <dbru997@gmail.com>
2021-03-11 19:28:41 -0700

    Level order traversal implemented to find array. Need to track path.

--- knight_moves.rb
+++ knight_moves.rb
@@ -1,30 +1,59 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 require 'YAML'
 
-class Square
-  attr_reader :coordinates
-  attr_accessor :pointers
+KNIGHT_MOVES = YAML.load_file('./knight_moves.yml')
+CHESS_BOARD = YAML.load_file('./chess_board.yml')
 
-  def initialize(coordinates)
-    @coordinates = coordinates
-    @pointers = []
-  end
+def print_knight_hash
+  KNIGHT_MOVES.each { |k, v| puts ":#{k} => #{v}" }
 end
 
-class Knight
-  @@BOARD = YAML.load_file('./board.yml')
-
-  def self.board
-    @@BOARD
+def print_chess_board
+  i = CHESS_BOARD.length - 1
+  until i == -1
+    j = i - 7
+    a = []
+    until j == i
+      a << CHESS_BOARD[j]
+      j += 1
+    end
+    puts a.join('  ')
+    i -= 8
   end
-
-  attr_accessor :current_spot
-
-  def initialize
-    @current_spot = :a1
-  end
+  p CHESS_BOARD
 end
 
-knite = Knight.new
-p Knight.board
+def valid_spot?(spot)
+  CHESS_BOARD.include?(spot)
+end
+
+def validate_spots?(start, finish)
+  unless valid_spot?(start) && valid_spot?(finish)
+    puts "Invalid parameters. Use a symbol, :a1 - :h8."
+    return false
+  end
+  true
+end
+
+def level_order(current_spot, desired_spot, queue=[current_spot], steps=[current_spot])
+  if current_spot == desired_spot
+    p steps
+    return
+  end
+  
+  next_spot_to_check = queue.shift
+
+  KNIGHT_MOVES[current_spot].each { |ref| queue << ref }
+  level_order(next_spot_to_check, desired_spot, queue, steps)
+end
+
+def print_knight_moves(start, finish)
+  return unless validate_spots?(start, finish)
+  level_order(start, finish)
+end
+
+# print_knight_hash
+# print_chess_board
+
+print_knight_moves(:a1, :h8)
